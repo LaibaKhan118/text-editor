537e51cb8318cf8255d8d32b192a738b5db6db73
Laiba Khan <laibawajahat123@gmail.com>
2025-07-11 03:18:51 +0500

    Added functionality to move the cursor with WASD keys

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@
 /*** Data ***/
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;
@@ -159,7 +160,10 @@ void editorRefreshScreen() {
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
@@ -167,6 +171,23 @@ void editorRefreshScreen() {
 }
 
 /*** Input ***/
+
+void editorMoveCursor(char key) {
+    switch(key) {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
+}
 
 void editorProcessKeypress() {
     char c = editorReadKey();
@@ -177,12 +198,20 @@ void editorProcessKeypress() {
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
+        case 'w':
+        case 'a':
+        case 's':
+        case 'd':
+            editorMoveCursor(c);
+            break;
     }
 }
 
 /*** Init ***/
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) { die("getWindowSize"); }
 }
 

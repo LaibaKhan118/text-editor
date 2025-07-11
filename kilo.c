7f8e0ffcaf996f79fa5b7f86534c2338d201b8e9
Laiba Khan <laibawajahat123@gmail.com>
2025-07-11 21:19:08 +0500

    Added Vertical Scrolling

--- kilo.c
+++ kilo.c
@@ -40,6 +40,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
     int screenrows;
     int screencols;
     int numrows;
@@ -225,10 +226,21 @@ void abFree(struct abuf *ab) {
 
 /*** Output ***/
 
+void editorScroll() {
+    if(E.cy < E.rowoff) {
+        E.rowoff = E.cy;
+    }
+
+    if(E.cy >= E.rowoff + E.screenrows) {
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     for(y = 0; y < E.screenrows; y++) {
-        if (y >= E.numrows) {
+        int filerow = y + E.rowoff;
+        if (filerow >= E.numrows) {
             if ((E.numrows == 0) && y == E.screenrows / 3) {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo Editor -- Version %s", KILO_VERSION);
@@ -248,9 +260,9 @@ void editorDrawRows(struct abuf *ab) {
             }
         }
         else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len > E.screencols) { len = E.screencols; }
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -261,6 +273,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -269,7 +283,7 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -292,7 +306,7 @@ void editorMoveCursor(int key) {
             if(E.cy != 0) { E.cy--; }
             break;
         case ARROW_DOWN:
-            if(E.cy != E.screenrows-1) { E.cy++; }
+            if(E.cy < E.numrows) { E.cy++; }
             break;
     }
 }
@@ -338,6 +352,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
 

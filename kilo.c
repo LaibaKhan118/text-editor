2f97d48849d14c533af80c2bd0edb99c4206eafd
Laiba Khan <laibawajahat123@gmail.com>
2025-07-15 23:18:59 +0500

    Adding search functionality

--- kilo.c
+++ kilo.c
@@ -268,7 +268,7 @@ int getWindowSize(int *rows, int *cols)
 int editorRowCxToRx(erow *row, int cx)
 {
     int rx = 0;
-    int j = 0;
+    int j;
     for (j = 0; j < cx; j++)
     {
         if (row->chars[j] == '\t')
@@ -278,6 +278,24 @@ int editorRowCxToRx(erow *row, int cx)
         rx++;
     }
     return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx)
+{
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++)
+    {
+        if (row->chars[cx] == '\t')
+        {
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        }
+        cur_rx++;
+
+        if (cur_rx > rx)
+            return cx;
+    }
+    return cx;
 }
 
 void editorUpdateRow(erow *row)
@@ -524,6 +542,30 @@ void editorSave()
     }
     editorSetStatusMessage("Can't Save! I/ O Error: %s", strerror(errno));
     free(buf);
+}
+
+/*** Find ***/
+
+void editorFind()
+{
+    char *query = editorPrompt("Search (ESC to cancel): %s");
+    if (query == NULL)
+        return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++)
+    {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match)
+        {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            return;
+        }
+    }
+    free(query);
 }
 
 /*** Append Buffer ***/
@@ -856,6 +898,10 @@ void editorProcessKeypress()
         }
         break;
 
+    case CTRL_KEY('f'):
+        editorFind();
+        break;
+
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -938,7 +984,7 @@ int main(int argc, char *argv[])
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1)
     {
@@ -948,4 +994,4 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// Resume: Error handling (step 122). Open Ubunto and cd to text-editor
+// Resume: Error handling (step 131). Open Ubunto and cd to text-editor
